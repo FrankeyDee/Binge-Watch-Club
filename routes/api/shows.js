c4de065630a2bec924c8410ce9b72ad30c7f7064
Val <50930320+water96763@users.noreply.github.com>
2019-11-07 18:27:48 -0800

    added mern package json in scr client

--- routes/api/shows.js
+++ routes/api/shows.js
@@ -1,8 +1,22 @@
 const router = require("express").Router();
 const showController = require("../../controllers/showController");
 
-router.route("/")
-///.get and .post here///
 
+// Matches with "/api/shows"
+router.route("/")
+  .get(showController.findAll)
+  //this findAll means all shows for one unique person
+  .post(showController.create);
+//this create is to create a show to save to the database
+
+// We want the app to save shows to a database, and be able to delete them after watching.  Do we need a .put?
+
+// Matches with "/api/shows/:id"
+// router
+  .route("/:id")
+  .get(showController.findById)
+  .put(showController.update)
+  .delete(showController.remove);
 
 module.exports = router;
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const showRoutes = require("./api/shows");
 
 //API Routes
 router.use("/api", apiRoutes);
+router.use('/shows', showRoutes);
 
 module.exports = router;
