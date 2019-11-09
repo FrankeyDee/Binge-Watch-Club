6cbe26e822d209daa340329ef619124a9420f1e4
frankey darban <frankeydarban@gmail.com>
2019-11-09 11:32:09 -0800

    updated nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Nav() {
   return (
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Binge Watch Club</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -14,13 +14,13 @@ function Nav() {
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Recommended</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">Log In</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Register</a>
       </li>
     </ul>
       <form class="form-inline my-2 my-lg-0">
