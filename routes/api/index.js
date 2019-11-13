7c3c90157fa2c49066438921a88e5978c8abe9ae
Val <50930320+water96763@users.noreply.github.com>
2019-11-12 20:59:10 -0800

    got rid of simmilar shows route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const showRoutes = require("./shows");
+
 const authRoutes = require("./auth");
 
 //Show Routes
 router.use("/auth", authRoutes);
-router.use("/shows", showRoutes);
+
 
 module.exports = router;
