2a75e9ee6d614cf0506df6728b2f97ffa927ad36
Val <50930320+water96763@users.noreply.github.com>
2019-11-12 19:34:34 -0800

    route show changes

--- controllers/showController.js
+++ controllers/showController.js
@@ -1,5 +1,38 @@
 const db = require("../models");
 
+
+// Defining methods for the booksController
 module.exports = {
-    //either Find All, Find by ID, Create, or Update functions go here//
-}
+  findAll: function(req, res) {
+    db.Show
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Show
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Show
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Show
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Show
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
