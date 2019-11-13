09ba7343c29194750494f567513faac76fc21720
frankey darban <frankeydarban@gmail.com>
2019-11-12 19:04:27 -0800

    updated nav links, bg and slider cards

---
+++ client/src/components/Home/about.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+
+export class About extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+
+                    <h1>HELLOOOOOOOO</h1>
+            </div>
+
+        );
+    }
+
+}
+
+export default About;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,2 +1,3 @@
 export { Home } from "./banner";
 export { Slider } from "./slider";
+export { About } from "./about";
