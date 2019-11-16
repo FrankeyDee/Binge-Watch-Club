ee2612fc5b2ea8016ddc2d4eba8b566a60466953
frankey darban <frankeydarban@gmail.com>
2019-11-16 02:19:44 -0800

    added word group image

--- client/src/components/Home/banner.js
+++ client/src/components/Home/banner.js
@@ -21,13 +21,23 @@ export class Home extends React.Component {
     render() {
         return (
             <div className="contain">
-                    <h1>Welcome to Binge Watch Club!</h1>
-                        <p className="lead text-primary">Find titles of the most recommended shows to "binge watch." Registration allows you to search by mood and save your personalized list of shows you would like to watch. So, grab that popcorn and find your spot on the couch. What are we watching?</p>
-                <br></br>
-                <br></br>
+                <div className="intro">
+                    <div className="wordart">
+                        <img id="wordart" src="tv_word_art.jpg" />
+                        <div className="banner">
+                            <h1>Welcome to The Binge Watch Club!</h1>
+                            <br></br>
+                            <br></br>
+                            <p className="lead text-primary">Find titles of the most recommended shows to "binge watch." Registration allows you to search by mood and save your personalized list of shows you would like to watch. So, grab that popcorn and find your spot on the couch. What are we watching?</p>
+                        </div>
+                    </div>
+                </div>
+                
 
-                <Title className="glow-edges">Recent Recommendations</Title>
-               
+
+                <div className="recent">
+                <Title>Recently Recommended</Title>
+                </div>
                 <br></br>
                
                <Slider />
