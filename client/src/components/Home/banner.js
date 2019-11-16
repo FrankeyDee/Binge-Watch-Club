978aee385ad4fb200ebf49a72371dc3d151b8923
frankey darban <frankeydarban@gmail.com>
2019-11-16 00:30:59 -0800

    included title, changed api search query to only provide 'show' results, styled slider buttons

--- client/src/components/Home/banner.js
+++ client/src/components/Home/banner.js
@@ -5,6 +5,8 @@
 import React from "react";
 import "./index.css";
 import Slider from "./slider";
+import Title from "../UsernameForm/Title";
+
 
 // import React, { useState } from 'react';
 // import ItemsCarousel from 'react-items-carousel';
@@ -19,12 +21,17 @@ export class Home extends React.Component {
     render() {
         return (
             <div className="contain">
-
-                <div className="jumbotron">
                     <h1>Welcome to Binge Watch Club!</h1>
-                        <p className="lead text-primary">Find titles of the most recommended shows to "binge watch." Registration allows you to search by mood and save your personalized list of shows you would like to binge. So, grab that popcorn and find your spot on the couch. It's time to binge watch!</p>
-                </div>
-                <Slider />
+                        <p className="lead text-primary">Find titles of the most recommended shows to "binge watch." Registration allows you to search by mood and save your personalized list of shows you would like to watch. So, grab that popcorn and find your spot on the couch. What are we watching?</p>
+                <br></br>
+                <br></br>
+
+                <Title className="glow-edges">Recent Recommendations</Title>
+               
+                <br></br>
+               
+               <Slider />
+            
             </div>
 
         );
