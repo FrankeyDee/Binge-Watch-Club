aa10822f2feb6816b811a9cf616ad5fa8b2945be
lax <czjordan323@gmail.com>
2019-11-16 14:01:36 -0800

    changed api to tmdb and also hid the api key. Then moved our call to the api key to the back end instead of the front end. Cards currently to small for images

--- client/src/components/Home/slider.js
+++ client/src/components/Home/slider.js
@@ -17,16 +17,16 @@ import "pure-react-carousel/dist/react-carousel.es.css";
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ query: 'buffy+slayer' })
+            body: JSON.stringify({ query: '35' })
         })
             .then(res => res.json())
-            .then(res => res.Search)
-            .then(res => res.reduce((movieObj, movie) => {
-                movieObj.push({ title: movie.Title, poster: movie.Poster, year: movie.Year, plot: movie.Plot });
+            .then(res => res.results)
+            .then(res => res.reduce((movieObj, show) => {
+                movieObj.push({ name: show.name, poster: 'https://image.tmdb.org/t/p/w400' + show.poster_path, overview: show.overview });
 
                 return movieObj;
             }, []))
-            .then(movies => this.setState({ movies: movies }))
+            .then(show => this.setState({ movies: show }))
             .catch(err => console.error(err))
     }
 
@@ -41,13 +41,13 @@ import "pure-react-carousel/dist/react-carousel.es.css";
                 <Slider>
                     <div className="card-container">
                     <ButtonNext className="btn-outline-secondary">&lt;&lt;</ButtonNext>
-
+                    {/* {console.log(this.state.movies)} */}
                     {this.state.movies.map(item => (
                         <div className="card text-white bg-dark mb-3 cardy">
-                            <div className="card-header">{item.year}</div>
+                            <div className="card-header">{item.name}</div>
                             <div className="card-body text-white">
-                                <img src={item.poster} alt={item.title} />
-                                <h4 className="text-white">{item.title}</h4>
+                                <img src={item.poster} alt={item.name} />
+                                <h4 className="text-white">{item.overview}</h4>
                             </div>
                         </div>
                     ))}
--- routes/api/index.js
+++ routes/api/index.js
@@ -8,7 +8,19 @@ router.use("/auth", authRoutes);
 
 router.route('/show/search')
     .post((req, res) => {
-        fetch(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${req.body.query}&type=series&plot=short`)
+        // fetch(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${req.body.query}&type=series&plot=short`)
+        //     .then(r => r.json())
+        //     .then(r => res.json(r))
+        //     .catch(err => res.status(500).json({ error: err }))
+        // fetch(`https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=${encodeURIComponent(req.body.query)}`, {
+        //     "method": "GET",
+        //     "headers": {
+        //         "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+        //         "x-rapidapi-key": process.env.RAPID_API_IMDB_API_KEY
+        //     }
+        // })
+        fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.TMDB_API_KEY}&language=en-US&with_genres=${req.body.query}`)
+
             .then(r => r.json())
             .then(r => res.json(r))
             .catch(err => res.status(500).json({ error: err }))
