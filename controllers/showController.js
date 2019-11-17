e06d357bd524f79fd36e6b80416caf6366605d03
Val <50930320+water96763@users.noreply.github.com>
2019-11-16 21:53:29 -0800

    added subdocument, deleted models, reroute to models-auth

--- controllers/showController.js
+++ controllers/showController.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require("../models-test-auth");
 
 
 // Defining methods for the booksController
--- models-test-auth/User.js
+++ models-test-auth/User.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
+const showSchema = new Schema({
+  name: {type: String, required: true},
+  poster: {type: String},
+  overview: {type: String}
+}
+
+)
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -11,8 +17,9 @@ const UserSchema = new Schema({
   hash: {
     type: String,
     required: true
-  }
-})
+  },
+  show: showSchema
+});
 
 const User = mongoose.model('User', UserSchema);
 
