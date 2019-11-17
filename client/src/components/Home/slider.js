4df6a9bee130eeaafc91a64023e833fef3e4b2c9
Val <50930320+water96763@users.noreply.github.com>
2019-11-16 22:08:04 -0800

    added schema subdocument and fixed card size and font import css file

--- client/src/components/Home/slider.js
+++ client/src/components/Home/slider.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import "pure-react-carousel/dist/react-carousel.es.css";
+import "./slider.css"
 
   export class Movies extends React.Component {
 
@@ -34,9 +35,9 @@ import "pure-react-carousel/dist/react-carousel.es.css";
         return (
  
             <CarouselProvider
-            naturalSlideHeight={125}
-            naturalSlideWidth={100}
-            totalSlides={2}>
+            naturalSlideHeight={200}
+            naturalSlideWidth={400}
+            totalSlides={12}>
  
                 <Slider>
                     <div className="card-container">
