253d931ea50e501d52984cb2ff64d8630df34d87
Val <50930320+water96763@users.noreply.github.com>
2019-11-19 19:45:48 -0800

    stash fix apply

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,3 +1,45 @@
-export { Home } from "./banner";
-export { Movies } from "./slider";
-export { About } from "./about";
+import React, { Component, Fragment } from "react";
+
+import Banner from "./banner";
+import MovieSlider from "../MovieSlider";
+import About from "./about";
+
+class Home extends Component {
+    state = {
+        movies: []
+    }
+    componentDidMount() {
+        this.loadMovies();
+    }
+
+    loadMovies = () => {
+        fetch('/api/show/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ query: '35' })
+        })
+            .then(res => res.json())
+            .then(res => res.results)
+            .then(res => res.reduce((movieObj, show) => {
+                movieObj.push({ name: show.name, poster: 'https://image.tmdb.org/t/p/w400' + show.poster_path, overview: show.overview });
+
+                return movieObj;
+            }, []))
+            .then(show => this.setState({ movies: show }))
+            .catch(err => console.error(err))
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Banner />
+                <MovieSlider movies={this.state.movies} saveHandler={(movie) => console.log('Should save at some point: ' + JSON.stringify(movie))} isSearch />
+                <About />
+            </Fragment>
+        );
+    }
+}
+
+export default Home
