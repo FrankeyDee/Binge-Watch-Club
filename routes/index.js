7d9af1a4158345948fd142f102dceb34e8bb9061
Val <50930320+water96763@users.noreply.github.com>
2019-11-23 10:08:46 -0800

    Try to fix routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-app.get('*', () => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html')); //relative path
 });
 
