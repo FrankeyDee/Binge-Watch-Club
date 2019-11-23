aa0e34221416ccd6298895eedb844f83995bcff2
frankey darban <frankeydarban@gmail.com>
2019-11-23 09:23:04 -0800

    updated static path to public path

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static("client/public"));
 }
 
 const sessConfig = {
